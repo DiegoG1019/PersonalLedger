867acbe7f3524fd027250f0269867ab0293f4dd5
Diego García <dagarciam1014@gmail.com>
2021-07-18 18:56:10 -0400

    Updated Dependencies and fixed several warnings

--- LedgerBase/Entry.cs
+++ LedgerBase/Entry.cs
@@ -1,5 +1,4 @@
-﻿using DiegoG.Utilities.Personal;
-using MessagePack;
+﻿using MessagePack;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ LedgerBase/Types/PhoneNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DiegoG.LedgerBase
+{
+    [Table("PhoneNumber")]
+    public class PhoneNumber
+    {
+        public int Id { get; set; }
+        
+        [DataType(DataType.PhoneNumber)]
+        public string? Number
+        {
+            get => NumberField;
+            set
+            {
+                if (value is null)
+                {
+                    NumberField = null;
+                    return;
+                }
+
+                var a = value.IndexOf(' ');
+                CountryCode = value[..a];
+                Line = value[(a + 1)..];
+                NumberField = value;
+            }
+        }
+
+        [DataType(DataType.PhoneNumber)]
+        private string? NumberField { get; set; }
+
+        public string? Comment { get; set; }
+
+        public string? CountryCode { get; private set; }
+        public string? Line { get; private set; }
+
+        public PhoneNumber(string number) => Number = number;
+        public PhoneNumber() { }
+    }
+}
