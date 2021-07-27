dadee1c11b11ac053feb128b9ae874324e97a277
Diego García <dagarciam1014@gmail.com>
2021-07-27 01:49:20 -0400

    Added support for EntityFramework & SQLite (Untested!)

---
+++ LedgerBase/CacheDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoG.LedgerBase
+{
+    public partial class CacheDbContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+
+        public CacheDbContext() : base(Directories.InCache("cache"))
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<CacheDbContext>());
+        }
+    }
+}
