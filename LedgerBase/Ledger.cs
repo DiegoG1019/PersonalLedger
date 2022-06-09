943bb8204de7abc5911615cbd53a4e27a1a51ce7
Diego García <dagarciam1014@gmail.com>
2022-06-09 18:48:49 -0400

    Create Ledger.cs

---
+++ LedgerBase/Ledger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoG.LedgerBase;
+
+/// <summary>
+/// Provides extension methods to configure things for PersonalLedger
+/// </summary>
+public static class LedgerExtensions
+{
+    public static IServiceCollection AddLedger(this IServiceCollection services)
+    {
+        services.AddSqlite<LedgerContext>(@"dat/ledger");
+        services.AddSingleton<ILedgerLanguage, StaticLedgerLanguage>();
+        return services;
+    }
+}
