493218deac35933eac5b26aeb6c4b3bbf60b4f3a
Diego García <dagarciam1014@gmail.com>
2022-06-09 18:30:31 -0400

    Renamed and modified Lang

---
+++ LedgerBase/Language.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace DiegoG.LedgerConsole;
+
+public class Language
+{
+    public string Lang { get; set; }
+    public string APIKeyReceived { get; set; } = "Received";
+    public string APIKeyQuery { get; set; } = "Please input your ExchangeRateAPI Key here";
+    public string AdquiredFrom { get; set; } = "By: {0}";
+    public string WithdrawnFor { get; set; } = "For: {0}";
+    public string DateTimeFormat { get; set; } = "MMM dd, hh:mmt";
+    public string UnknownPerson { get; set; } = "Unknown";
+}
