3596b0391020afbe8197a137fffc23be70b46f27
Diego García <dagarciam1014@gmail.com>
2022-06-09 18:48:41 -0400

    Create ILedgerLanguage.cs

---
+++ LedgerBase/ILedgerLanguage.cs
@@ -0,0 +1,11 @@
+﻿using DiegoG.LedgerConsole;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoG.LedgerBase;
+public interface ILedgerLanguage
+{
+    public Task<Language> GetLanguage(string lang);
+}
