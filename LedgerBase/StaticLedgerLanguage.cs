c2145486aac11e3e212625d3af98dc56db6545a9
Diego García <dagarciam1014@gmail.com>
2022-06-09 18:48:46 -0400

    Create StaticLedgerLanguage.cs

---
+++ LedgerBase/StaticLedgerLanguage.cs
@@ -0,0 +1,46 @@
+﻿using DiegoG.LedgerConsole;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DiegoG.LedgerBase;
+
+internal sealed class StaticLedgerLanguage : ILedgerLanguage
+{
+    public static readonly Dictionary<string, Task<Language>> langs = new()
+    {
+        { "eng", Task.FromResult(new Language
+            (
+                Lang: "eng",
+                APIKeyReceived: "API Key saved",
+                APIKeyQuery: "Please input your API Key",
+                Unknown: "Unknown",
+                Entry: new(
+                    "Date",
+                    "Added on",
+                    "Last edited on",
+                    "Description",
+                    "Comments",
+                    "Source"
+                ),
+                Business: new(
+                    "Alias",
+                    "First name",
+                    "Second name",
+                    "Comment",
+                    "Phone number",
+                    "Address",
+                    "E-mail",
+                    "Payment information"
+                ),
+                PaymentInfo: new(
+                    "Entity",
+                    "Title",
+                    "Comment",
+                    "Data"
+                )
+            )) 
+        }
+    };
+
+    public Task<Language> GetLanguage(string lang) => langs[lang];
+}
