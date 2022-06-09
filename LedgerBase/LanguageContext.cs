ea0cce68ed7a314130f7c469f48722b8b4192d11
Diego García <dagarciam1014@gmail.com>
2022-06-09 18:30:34 -0400

    Create LanguageContext.cs

---
+++ LedgerBase/LanguageContext.cs
@@ -0,0 +1,11 @@
+﻿using DiegoG.LedgerConsole;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiegoG.LedgerBase;
+
+public partial class LanguageContext : DbContext
+{
+    public DbSet<Language> Languages { get; set; }
+
+    public LanguageContext(DbContextOptions<LanguageContext> options) : base(options) { }
+}
