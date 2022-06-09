509a48076288e7fa7d647d83c88e2d0cc8888c96
Diego García <dagarciam1014@gmail.com>
2022-06-09 18:29:34 -0400

    Renamed and modified CacheDbContext

---
+++ LedgerBase/LedgerContext.cs
@@ -0,0 +1,33 @@
+﻿using DiegoG.LedgerBase.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoG.LedgerBase;
+
+public partial class LedgerContext : DbContext
+{
+    public DbSet<BusinessEntity> Businesses { get; set; }
+    public DbSet<Entry> Entries { get; set; }
+    public DbSet<PaymentInfo> PaymentInfo { get; set; }
+
+    public LedgerContext(DbContextOptions<LedgerContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        base.OnModelCreating(mb);
+
+        var business = mb.Entity<BusinessEntity>();
+        business.HasKey(x => x.Alias);
+        business.OwnsMany(x => x.PaymentInfo).WithOwner(x => x.Entity);
+
+        var entry = mb.Entity<Entry>();
+        entry.HasKey(x => x.Id);
+
+        var payments = mb.Entity<PaymentInfo>();
+        payments.HasKey(x => x.Id);
+    }
+}
